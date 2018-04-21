4e7241dfa468ce2668dac53859992e71be5f3e5b
nakatsuma <>
2018-04-21 10:24:04 +0900

    会員モデルとそのバリデーションの追加

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,11 @@
 class Account < ApplicationRecord
+  before_save {self.email.downcase!}
+
+  validates :last_name, presence: true, length: {maximum: 50}
+  validates :first_name, presence: true, length: {maximum: 50}
+  VALID_USER_NAME_REGEX = /[A-Za-z\d.]+/
+  validates :user_name, presence: true, length: {in: 5..15}, format: {with: VALID_USER_NAME_REGEX}, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  has_secure_password
 end
--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,7 +1,130 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @account = Account.new(last_name: "Sample",
+                           first_name: "Test",
+                           user_name: "Test.01",
+                           email: "account@example.com",
+                           password: "foobar",
+                           password_confirmation: "foobar")
+  end
+
+  test "should be valid" do
+    assert @account.valid?
+  end
+
+  test "last name should not be nil" do
+    @account.last_name = nil
+    assert_not @account.valid?
+  end
+
+  test "last name should not be blank" do
+    @account.last_name = "   "
+    assert_not @account.valid?
+  end
+
+  test "last name should not be over 50" do
+    @account.last_name = "a" * 51
+    assert_not @account.valid?
+  end
+
+  test "first name should not be nil" do
+    @account.first_name = nil
+    assert_not @account.valid?
+  end
+
+  test "first name should not be blank" do
+    @account.first_name = "   "
+    assert_not @account.valid?
+  end
+
+  test "first name should not be over 50" do
+    @account.first_name = "a" * 51
+    assert_not @account.valid?
+  end
+
+  test "user name should not be nil" do
+    @account.user_name = nil
+    assert_not @account.valid?
+  end
+
+  test "user name should not be blank" do
+    @account.user_name = "   "
+    assert_not @account.valid?
+  end
+
+  test "user name should not be too long and too short" do
+    @account.user_name = "a" * 16
+    assert_not @account.valid?
+    @account.user_name = "a" * 4
+    assert_not @account.valid?
+  end
+
+  test "user name should not be invalid char" do
+    @account.user_name = "`~!@\#$%^&*()"
+    assert_not @account.valid?
+    @account.user_name = "[]{}\\|;:'\",/"
+    assert_not @account.valid?
+    @account.user_name = "-_<>?=+あ阿α∀"
+    assert_not @account.valid?
+  end
+
+  test "user name should be unique" do
+    assert @account.save
+    same_user_name_account = @account.dup
+    same_user_name_account.user_name = "Test.01"
+    same_user_name_account.email = "account01@example.com"
+    assert_not same_user_name_account.valid?
+  end
+
+  test "email should not be nil" do
+    @account.email = nil
+    assert_not @account.valid?
+  end
+
+  test "email should not be blank" do
+    @account.email = "   "
+    assert_not @account.valid?
+  end
+
+  test "email should not be over 255" do
+    @account.email = ("a" * 244) + "@example.com"
+    assert_not @account.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @account.email = valid_address
+      assert @account.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @account.email = invalid_address
+      assert_not @account.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email should be unique" do
+    assert @account.save
+    same_email_account = @account.dup
+    same_email_account.user_name = "Test.02"
+    same_email_account.email = "account@example.com"
+    assert_not same_email_account.valid?
+    same_email_account.email.upcase!
+    assert_not same_email_account.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @account.email = mixed_case_email
+    @account.save
+    assert_equal mixed_case_email.downcase, @account.reload.email
+  end
+
 end
