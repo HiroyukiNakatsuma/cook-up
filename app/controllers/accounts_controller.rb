70e82e60cf5533c127636c97314c13f9ff58d099
nakatsuma <>
2018-04-22 12:10:13 +0900

    会員登録のバリデーションメッセージを追加

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,6 +5,18 @@ class AccountsController < ApplicationController
   end
 
   def create
+    @account = Account.new(account_params)
+    if @account.save
+      # 保存の成功。
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:last_name, :first_name, :user_name, :email, :password, :password_confirmation)
   end
 
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,12 +1,25 @@
 class Account < ApplicationRecord
   before_save {self.email.downcase!}
 
-  validates :last_name, presence: true, length: {maximum: 50}
-  validates :first_name, presence: true, length: {maximum: 50}
+  validates :last_name,
+            presence: {message: I18n.t("validations.blank")},
+            length: {maximum: 50, message: I18n.t("validations.over_length", max_length: 50)}
+  validates :first_name,
+            presence: {message: I18n.t("validations.blank")},
+            length: {maximum: 50, message: I18n.t("validations.over_length", max_length: 50)}
   VALID_USER_NAME_REGEX = /[A-Za-z\d.]+/
-  validates :user_name, presence: true, length: {in: 5..15}, format: {with: VALID_USER_NAME_REGEX}, uniqueness: true
+  validates :user_name,
+            presence: {message: I18n.t("validations.blank")},
+            length: {in: 5..15, message: I18n.t("validations.not_within_length_range", min_length: 5, max_length: 15)},
+            format: {with: VALID_USER_NAME_REGEX},
+            uniqueness: {message: I18n.t("validations.already_used")}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :email,
+            presence: {message: I18n.t("validations.blank")},
+            length: {maximum: 255, message: I18n.t("validations.over_length", max_length: 255)},
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false, message: I18n.t("validations.already_used")}
   has_secure_password
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password,
+            length: {minimum: 6, message: I18n.t("validations.not_enough_length", min_length: 6)}
 end
