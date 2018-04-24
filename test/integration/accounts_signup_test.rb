33fd0ae0e434b2a9ca901467c102711de3cba4eb
nakatsuma <>
2018-04-24 22:19:45 +0900

    会員登録成功時のテストコードを追加

--- test/integration/accounts_signup_test.rb
+++ test/integration/accounts_signup_test.rb
@@ -16,4 +16,19 @@ class AccountsSignupTest < ActionDispatch::IntegrationTest
     assert_select '#error_explanation .alert'
     assert_select '#error_explanation li', 6
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'Account.count', 1 do
+      post signup_path, params: {account: {last_name: "Example",
+                                           first_name: "Account",
+                                           user_name: "Test01",
+                                           email: "account@example.com",
+                                           password: "password01",
+                                           password_confirmation: "password01"}}
+    end
+    follow_redirect!
+    assert_template root_path
+    assert_not flash.empty?
+  end
 end
