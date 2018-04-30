613e737578530722f119c5eb26c50a8d46616baa
nakatsuma <>
2018-04-30 16:07:42 +0900

    remember-meの追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,15 +4,38 @@ module SessionsHelper
     session[:account_id] = account.id
   end
 
+  # ユーザーのセッションを永続的にする
+  def remember(account)
+    account.remember
+    cookies.permanent.signed[:account_id] = account.id
+    cookies.permanent[:remember_token] = account.remember_token
+  end
+
   def current_account
-    @current_account ||= Account.find_by(id: session[:account_id])
+    if (account_id = session[:account_id])
+      @current_account ||= Account.find_by(id: account_id)
+    elsif (account_id = cookies.signed[:account_id])
+      account = Account.find_by(id: account_id)
+      if account && account.authenticated?(cookies[:remember_token])
+        log_in account
+        @current_account = account
+      end
+    end
   end
 
   def logged_in?
     !current_account.nil?
   end
 
+  # 永続的セッションを破棄する
+  def forget(account)
+    account.forget
+    cookies.delete(:account_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    forget(current_account)
     session.delete(:account_id)
     @current_account = nil
   end
